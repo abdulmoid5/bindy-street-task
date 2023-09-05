cd310689032c5b77133984003b6fda4594d74c67
abdulmoid5 <abdulmoidbabar@gmail.com>
2023-09-05 11:29:57 +0100

    card view hook improved

--- src/screens/cardView/hooks/useCardView.ts
+++ src/screens/cardView/hooks/useCardView.ts
@@ -1,12 +1,19 @@
-import {useEffect, useMemo, useRef, useState} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import {useGetApi} from '~/hooks/api';
+
+const CARD_COUNT_PER_ROW = 10;
+const TOTAL_CARDS = 30;
 
 export const useCardView = () => {
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState({row1: [], row2: [], row3: []});
+  const [data, setData] = useState({
+    row1: [],
+    row2: [],
+    row3: [],
+  });
 
   const {
-    data: photos,
+    data: photos = [],
     loading: photosLoading,
     refetch,
   } = useGetApi('photos?_start=0&_limit=30');
@@ -14,19 +21,12 @@ export const useCardView = () => {
   useEffect(() => {
     if (photosLoading || !photos) return;
     setData({
-      row1: photos.slice(0, 10),
-      row2: photos.slice(10, 20),
-      row3: photos.slice(20, 30),
+      row1: photos.slice(0, CARD_COUNT_PER_ROW),
+      row2: photos.slice(CARD_COUNT_PER_ROW, CARD_COUNT_PER_ROW * 2),
+      row3: photos.slice(CARD_COUNT_PER_ROW * 2, TOTAL_CARDS),
     });
     setLoading(false);
   }, [photos, photosLoading]);
 
-  return useMemo(
-    () => ({
-      loading,
-      data,
-      refetch,
-    }),
-    [loading, data, refetch],
-  );
+  return useMemo(() => ({loading, data, refetch}), [loading, data, refetch]);
 };
